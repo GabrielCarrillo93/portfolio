cc0cc628fc4d408e2ab014c67f890f20763259b5
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2024-03-12 18:45:27 -0300

    layout desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AboutMe from './components/AboutMe/AboutMe'
+import Contacto from './components/Contacto/Contacto'
+import DescargarCV from './components/DescargarCV/DescargarCV'
+import Experiencia from './components/Experiencia/Experiencia'
+import Proyectos from './components/Proyectos/Proyectos'
+import Tecnologias from './components/Tecnologias/Tecnologias'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <main>
+                <AboutMe />
+                <Proyectos />
+                <Contacto />
+                <DescargarCV />
+                <Tecnologias />
+                <Experiencia />
+            </main>
+        </>
+    )
 }
 
 export default App
