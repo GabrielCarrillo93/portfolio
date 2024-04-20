e7f8d680fb610446d06ac4b8d452a9abca7bf021
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2024-04-20 18:43:13 -0300

    imagenes

---
+++ src/components/Certificados/Certificados.jsx
@@ -0,0 +1,10 @@
+import './Certificados.css'
+
+const Certificados = () => {
+  return (
+    <section className='certificados'>
+        <h1>Certificados</h1>
+    </section>
+  )
+}
+export default Certificados
