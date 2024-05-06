f1421989840d84da77ff8825001a38ded61e66bc
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2024-05-06 19:36:21 -0300

    mobile finished

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
     return (
         <section className="sobremi">
             <h1>Sobre Mí</h1>
-                <p>Soy un desarrollador en busca de mi primera inserción en el mundo IT. Si bien poseo conocimientos fullstack pero mi especialidad es el front end.</p>
+                <p>Soy un desarrollador en busca de mi primera inserción en el mundo IT. Si bien poseo conocimientos fullstack, mi especialidad es el desarrollo front end. ¡Te invito a conocerme!</p>
         </section>
     )
 }
--- src/components/Certificados/Certificados.jsx
+++ src/components/Certificados/Certificados.jsx
@@ -2,10 +2,16 @@
 import CardCertificado from '../CardCertificado/CardCertificado'
 import './Certificados.css'
 const Certificados = ({certificados}) => {
+    const handleScroll = (e) => {
+        const delta = Math.max(-1, Math.min(1, (e.nativeEvent.wheelDelta || -e.nativeEvent.detail)))
+        e.currentTarget.scrollLeft -= (delta * 50)
+        e.preventDefault()
+    }
+    
     return (
         <section className='certificados'>
             <h1>Certificados</h1>
-            <div className='lista'>
+            <div className='lista' onWheel={handleScroll}>
                 {certificados.toReversed().map((el, i) => <CardCertificado 
                     key={i}
                     nombre={el.titulo}
